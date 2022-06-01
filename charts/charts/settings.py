6d237846b63c077a00f7d6bd822a0bba56a44f5a
ravik0105 <90980276+ravik0105@users.noreply.github.com>
2022-06-01 17:43:54 +0530

    Update settings.py

--- charts/charts/settings.py
+++ charts/charts/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1jiq5p%aep_+_(mi$p_=sj!mh)^c!*ib10f%xu4go7ol%k!9&+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','cpsinvoiceapp.herokuapp.com']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
