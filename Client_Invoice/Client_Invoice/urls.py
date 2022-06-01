1bbcca206508a6e6f7a66bd7602ab311b79fca06
ravik0105 <90980276+ravik0105@users.noreply.github.com>
2022-06-01 16:51:41 +0530

    Update urls.py

--- Client_Invoice/Client_Invoice/urls.py
+++ Client_Invoice/Client_Invoice/urls.py
@@ -14,7 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
+from django.conf.urls.static import static
+from django.conf import settings
+from django.views.static import serve
 
 admin.site.site_header = 'cps Administration'
 admin.site.site_title = 'cps Administration'
@@ -25,7 +28,9 @@ urlpatterns = [
     path('',include('members.urls')),
     path('',include('Invoice.urls')),
     #path('members/',include('django.contrib.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
    
 ]
-
+urlpatterns=urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
